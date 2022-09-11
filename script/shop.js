e96f68fcbe42dab5ea196ea77e48d0d8e50cb712
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2022-09-12 02:09:02 +0530

    Add files via upload

---
+++ script/shop.js
@@ -0,0 +1,3 @@
+console.log('Connected');
+var softDrinkNames = ["Pisco Punch", "Corpse Reviver", "Irish Coffee", "Hanky-Panky"];
+var imgSrc = ["../img/PiscoPunch.png", "../img/CorpseReviver.png", "../img/IrishCoffee.png", "../img/HankyPanky.png"];
