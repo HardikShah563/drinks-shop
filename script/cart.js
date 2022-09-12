e13a9a1f812df1ad70e15e4122483a17288fa76c
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2022-09-13 01:00:45 +0530

    Add files via upload

--- script/cart.js
+++ script/cart.js
@@ -3,7 +3,7 @@ console.log('Connected');
 var addToCart = document.querySelector(".add-to-cart");
 let popup = document.querySelector(".popup");
 
-addToCart.addEventListener("click", function () {
+addToCart.addEventListener("click", () => {
     popup.classList.remove("hidden");
     setTimeout(fadeOut, 3000);
 });
